ab1aada09e10996b82e11791dd0712c94b0b03f2
travis <travis.x.wong@gmail.com>
2020-09-13 12:00:55 +0800

    选择框

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,12 @@
 <template>
   <div>
     <p>{{hobby}}</p>
-    <label>
-      抽烟<input type="radio" v-model="hobby" value="抽烟">
-    </label>
-    <label>
-      喝酒<input type="radio" v-model="hobby" value="喝酒">
-    </label>
-    <label>
-      烫头<input type="radio" v-model="hobby" value="烫头">
-    </label>
+    <select name="hobby" v-model="hobby">
+      <option disabled value="">--</option>
+      <option>抽烟</option>
+      <option>喝酒</option>
+      <option>烫头</option>
+    </select>
   </div>
 </template>
 
